d94bc2cb74f6ec5f89f2b1779869a194f8081355
Eyck-Alexander Jentzsch <alex@minres.com>
2025-01-08 13:47:04 +0100

    updates coredsl2 implementation

---
+++ coredsl2_utils.py
@@ -0,0 +1,267 @@
+from constants import arg_lut
+from shared_utils import InstrDict
+
+
+def map_variable(variable_name: str):
+    # basically the arg_lut table, formated using ChatGPT
+    variable_mapping = {
+        "rd": "rd[4:0]",
+        "rt": "rt",
+        "rs1": "rs1[4:0]",
+        "rs2": "rs2[4:0]",
+        "rs3": "rs3",
+        "aqrl": "aqrl",
+        "aq": "aq[0:0]",
+        "rl": "rl[0:0]",
+        "imm20": "imm[31:12]",
+        "jimm20": "imm[20:20] :: imm[10:1] :: imm[11:11] :: imm[19:12]",
+        "imm12": "imm[11:0]",
+        "csr": "csr[11:0]",
+        "imm12hi": "imm[11:5]",
+        "bimm12hi": "imm[12:12] :: imm[10:5]",
+        "imm12lo": "imm[4:0]",
+        "bimm12lo": "imm[4:1] :: imm[11:11]",
+        "shamtq": "shamtq",
+        "shamtw": "shamtw",
+        "shamtw4": "shamtw4",
+        "shamtd": "shamtd",
+        "bs": "bs",
+        "rnum": "rnum",
+        "rc": "rc",
+        "imm2": "imm[1:0]",
+        "imm3": "imm[2:0]",
+        "imm4": "imm[3:0]",
+        "imm5": "imm[4:0]",
+        "imm6": "imm[5:0]",
+        "zimm": "zimm[4:0]",
+        "opcode": "opcode",
+        "funct7": "funct7",
+        "vd": "vd",
+        "vs3": "vs3",
+        "vs1": "vs1",
+        "vs2": "vs2",
+        "vm": "vm",
+        "wd": "wd",
+        "amoop": "amoop",
+        "nf": "nf",
+        "simm5": "simm5",
+        "zimm5": "zimm5",
+        "zimm10": "zimm10",
+        "zimm11": "zimm11",
+        "zimm6hi": "zimm6hi",
+        "zimm6lo": "zimm6lo",
+        "c_nzuimm10": "imm[5:4] :: imm[9:6] :: imm[2:2] :: imm[3:3]",
+        "c_uimm7lo": "uimm[2:2] :: uimm[6:6]",
+        "c_uimm7hi": "uimm[5:3]",
+        "c_uimm8lo": "c_uimm8lo",
+        "c_uimm8hi": "c_uimm8hi",
+        "c_uimm9lo": "c_uimm9lo",
+        "c_uimm9hi": "c_uimm9hi",
+        "c_nzimm6lo": "imm[4:0]",
+        "c_nzimm6hi": "imm[5:5]",
+        "c_imm6lo": "imm[4:0]",
+        "c_imm6hi": "imm[5:5]",
+        "c_nzimm10hi": "nzimm[9:9]",
+        "c_nzimm10lo": "nzimm[4:4] :: nzimm[6:6] :: nzimm[8:7] :: nzimm[5:5]",
+        "c_nzimm18hi": "imm[17:17]",
+        "c_nzimm18lo": "imm[16:12]",
+        "c_imm12": "imm[11:11] :: imm[4:4] :: imm[9:8] :: imm[10:10] :: imm[6:6] :: imm[7:7] :: imm[3:1] :: imm[5:5]",
+        "c_bimm9lo": "imm[7:6] :: imm[2:1] :: imm[5:5]",
+        "c_bimm9hi": "imm[8:8] :: imm[4:3]",
+        "c_nzuimm5": "c_nzuimm5",
+        "c_nzuimm6lo": "c_nzuimm6lo",
+        "c_nzuimm6hi": "c_nzuimm6hi",
+        "c_uimm8splo": "uimm[4:2] :: uimm[7:6]",
+        "c_uimm8sphi": "uimm[5:5]",
+        "c_uimm8sp_s": "uimm[5:2] :: uimm[7:6]",
+        "c_uimm10splo": "c_uimm10splo",
+        "c_uimm10sphi": "c_uimm10sphi",
+        "c_uimm9splo": "c_uimm9splo",
+        "c_uimm9sphi": "c_uimm9sphi",
+        "c_uimm10sp_s": "c_uimm10sp_s",
+        "c_uimm9sp_s": "c_uimm9sp_s",
+        "c_uimm2": "uimm[0:0] :: uimm[1:1]",
+        "c_uimm1": "uimm[1:1]",
+        "c_rlist": "rlist[3:0]",
+        "c_spimm": "spimm[5:4]",
+        "c_index": "c_index",
+        "rs1_p": "rs1[2:0]",
+        "rs2_p": "rs2[2:0]",
+        "rd_p": "rd[2:0]",
+        "rd_rs1_n0": "rs1[4:0]",
+        "rd_rs1_p": "rd[2:0]",
+        "rd_rs1": "rs1[4:0]",
+        "rd_n2": "rd[4:0]",
+        "rd_n0": "rd_n0",
+        "rs1_n0": "rs1[4:0]",
+        "c_rs2_n0": "rs2[4:0]",
+        "c_rs1_n0": "rs1[4:0]",
+        "c_rs2": "rs2[4:0]",
+        "c_sreg1": "rs1[2:0]",
+        "c_sreg2": "rs2[2:0]",
+        "fm": "fm[3:0]",
+        "pred": "pred[3:0]",
+        "succ": "succ[3:0]",
+        "rm": "rm[2:0]",
+    }
+
+    return variable_mapping.get(variable_name, "Entry not in variable_mapping")
+
+
+def map_assembly(variable_name: str):
+    variable_mapping = {
+        "rd": "name(rd)",
+        "rt": "rt",
+        "rs1": "name(rs1)",
+        "rs2": "name(rs2)",
+        "rs3": "rs3",
+        "aqrl": "aqrl",
+        "aq": "aq[0:0]",
+        "rl": "rl[0:0]",
+        "imm20": "imm[31:12]",
+        "jimm20": "imm[20:20] :: imm[10:1] :: imm[11:11] :: imm[19:12]",
+        "imm12": "imm",
+        "csr": "csr",
+        "imm12hi": "",
+        "bimm12hi": "",
+        "imm12lo": "imm",
+        "bimm12lo": "imm",
+        "shamtq": "shamtq",
+        "shamtw": "shamtw",
+        "shamtw4": "shamtw4",
+        "shamtd": "shamtd",
+        "bs": "bs",
+        "rnum": "rnum",
+        "rc": "rc",
+        "imm2": "imm[1:0]",
+        "imm3": "imm[2:0]",
+        "imm4": "imm[3:0]",
+        "imm5": "imm[4:0]",
+        "imm6": "imm[5:0]",
+        "zimm": "zimm",
+        "opcode": "opcode",
+        "funct7": "funct7",
+        "vd": "vd",
+        "vs3": "vs3",
+        "vs1": "vs1",
+        "vs2": "vs2",
+        "vm": "vm",
+        "wd": "wd",
+        "amoop": "amoop",
+        "nf": "nf",
+        "simm5": "simm",
+        "zimm5": "zimm",
+        "zimm10": "zimm",
+        "zimm11": "zimm",
+        "zimm6hi": "zimm6hi",
+        "zimm6lo": "zimm6lo",
+        "c_nzuimm10": "imm[5:4] :: imm[9:6] :: imm[2:2] :: imm[3:3]",
+        "c_uimm7lo": "uimm[2:2] :: uimm[6:6]",
+        "c_uimm7hi": "uimm[5:3]",
+        "c_uimm8lo": "c_uimm8lo",
+        "c_uimm8hi": "c_uimm8hi",
+        "c_uimm9lo": "c_uimm9lo",
+        "c_uimm9hi": "c_uimm9hi",
+        "c_nzimm6lo": "imm[4:0]",
+        "c_nzimm6hi": "imm[5:5]",
+        "c_imm6lo": "imm[4:0]",
+        "c_imm6hi": "imm[5:5]",
+        "c_nzimm10hi": "nzimm[9:9]",
+        "c_nzimm10lo": "nzimm[4:4] :: nzimm[6:6] :: nzimm[8:7] :: nzimm[5:5]",
+        "c_nzimm18hi": "imm[17:17]",
+        "c_nzimm18lo": "imm[16:12]",
+        "c_imm12": "imm[11:11] :: imm[4:4] :: imm[9:8] :: imm[10:10] :: imm[6:6] :: imm[7:7] :: imm[3:1] :: imm[5:5]",
+        "c_bimm9lo": "imm[7:6] :: imm[2:1] :: imm[5:5]",
+        "c_bimm9hi": "imm[8:8] :: imm[4:3]",
+        "c_nzuimm5": "c_nzuimm5",
+        "c_nzuimm6lo": "c_nzuimm6lo",
+        "c_nzuimm6hi": "c_nzuimm6hi",
+        "c_uimm8splo": "uimm[4:2] :: uimm[7:6]",
+        "c_uimm8sphi": "uimm[5:5]",
+        "c_uimm8sp_s": "uimm[5:2] :: uimm[7:6]",
+        "c_uimm10splo": "c_uimm10splo",
+        "c_uimm10sphi": "c_uimm10sphi",
+        "c_uimm9splo": "c_uimm9splo",
+        "c_uimm9sphi": "c_uimm9sphi",
+        "c_uimm10sp_s": "c_uimm10sp_s",
+        "c_uimm9sp_s": "c_uimm9sp_s",
+        "c_uimm2": "uimm[0:0] :: uimm[1:1]",
+        "c_uimm1": "uimm[1:1]",
+        "c_rlist": "rlist[3:0]",
+        "c_spimm": "spimm[5:4]",
+        "c_index": "c_index",
+        "rs1_p": "rs1[2:0]",
+        "rs2_p": "rs2[2:0]",
+        "rd_p": "rd[2:0]",
+        "rd_rs1_n0": "rs1[4:0]",
+        "rd_rs1_p": "rd[2:0]",
+        "rd_rs1": "rs1[4:0]",
+        "rd_n2": "rd[4:0]",
+        "rd_n0": "rd_n0",
+        "rs1_n0": "rs1[4:0]",
+        "c_rs2_n0": "rs2[4:0]",
+        "c_rs1_n0": "rs1[4:0]",
+        "c_rs2": "rs2[4:0]",
+        "c_sreg1": "rs1[2:0]",
+        "c_sreg2": "rs2[2:0]",
+        "fm": "fm[3:0]",
+        "pred": "pred[3:0]",
+        "succ": "succ[3:0]",
+        "rm": "rm[2:0]",
+    }
+    return variable_mapping.get(variable_name, "Entry not in assembly_mapping")
+
+
+def make_coredsl2(instr_dict: InstrDict) -> None:
+
+    for inst_name, instr_info in instr_dict.items():
+        enc = instr_info["encoding"]
+        coredsl_enc = ""
+
+        literal_builder = ""
+        for pos, token in enumerate(enc):
+            # build up the literal
+            if token == "1" or token == "0":
+                literal_builder += token
+            else:
+                # flush the builder
+                if literal_builder:
+                    coredsl_enc += f"{len(literal_builder)}'b{literal_builder} :: "
+                    literal_builder = ""
+                # check for variables
+                for variable in instr_info["variable_fields"]:
+                    variable_encoding = arg_lut[variable]
+                    if len(enc) - pos - 1 == variable_encoding[0]:
+                        coredsl_enc += f"{map_variable(variable)} :: "
+        # flush builder at the end of encoding
+        coredsl_enc += f"{len(literal_builder)}'b{literal_builder}"
+
+        assembly = ""
+        for pos, variable in enumerate(instr_info["variable_fields"]):
+            assembly_entry = map_assembly(variable)
+            # incase there's a two part variable skip emitting the skeleton
+            if not assembly_entry:
+                continue
+            assembly += "{"
+            assembly += f"{assembly_entry}"
+            assembly += "}"
+            if pos != len(instr_info["variable_fields"]) - 1:
+                assembly += ", "
+
+        to_check = ["rd", "rs1", "rs2"]
+        rfs_checks = [f"{reg} >=RFS" for reg in to_check if reg in coredsl_enc]
+
+        if rfs_checks:
+            behavior = f'if({ " || ".join(rfs_checks)}) raise(0, 2);\n'
+            behavior += "              else "
+        else:
+            behavior = ""
+        if "rd" in coredsl_enc:
+            behavior += "if(rd != 0)"
+        behavior += "{\n              }"
+        print(f"{inst_name.upper().replace('_','__')} {{")
+        print(f"    encoding: {coredsl_enc};")
+        if assembly:
+            print(f'    assembly: "{assembly}";')
+        print(f"    behavior: {behavior}")
+        print("}")
